6d743b903764fa9026b06b9e9638a59456431f85
Antonio Gamero <125345806+nono343@users.noreply.github.com>
2023-07-18 09:25:10 +0000

    second try

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 import { Context } from "./store/appContext";
@@ -13,15 +13,12 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
 const Layout = () => {
     const { store, actions } = useContext(Context);
 
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL === "") return <BackendURL />;
 
     return (
         <div>
@@ -29,11 +26,14 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<SignUp />} path="/signup" />
-                        <Route path="/private" element={store.isAuthenticated ? <Private /> : null} />
-                        <Route element={<LogIn />} path="/login" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route
+                            path="/private"
+                            element={store.isAuthenticated ? <Private /> : <Navigate to="/login" replace={true} />}
+                        />
+                        <Route path="/login" element={<LogIn />} />
+                        <Route path="*" element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
